478d48f3a7e3312d0471af876b8fdb12bb75d290
ViktorPisarczyk <viktor.pisarczyk@gmail.com>
2025-02-10 11:16:59 +0100

    userModel rework and finish

---
+++ so.dev/BE/models/userModel.js
@@ -0,0 +1,77 @@
+import { Schema, model } from "mongoose";
+import bcrypt from "bcrypt";
+
+const userSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  firstName: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    lowercase: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  profilePicture: {
+    type: String,
+    default: "",
+  },
+  bio: {
+    type: String,
+    maxlength: 300, // Limit bio to 300 characters
+    default: "",
+  },
+  location: {
+    type: String,
+    default: "",
+  },
+  posts: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Post",
+    },
+  ],
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+// Hash password before saving
+userSchema.pre("save", async function (next) {
+  try {
+    if (!this.isModified("password")) {
+      return next();
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
+
+// Authenticate user
+userSchema.methods.auth = async function (plainTextPass) {
+  return await bcrypt.compare(plainTextPass, this.password);
+};
+
+export const User = model("User", userSchema);
