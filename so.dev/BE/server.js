0df3f7ef338fcf7c1e1e19f701cc52aa522d8a9b
contact91 <chouvelquentin91@gmail.com>
2025-02-27 13:42:48 +0100

    host changed to PORT 5001

--- so.dev/BE/server.js
+++ so.dev/BE/server.js
@@ -25,7 +25,7 @@ app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 // const __dirname = dirname(fileURLToPath(import.meta.url));
 
