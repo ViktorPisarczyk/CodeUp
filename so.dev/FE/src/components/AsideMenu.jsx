59fee948055166e79df52d0e29d9bf935844ed68
ViktorPisarczyk <viktor.pisarczyk@gmail.com>
2025-03-10 15:18:58 +0100

    Added profilePicture and links to the sidebar

--- so.dev/FE/src/components/AsideMenu.jsx
+++ so.dev/FE/src/components/AsideMenu.jsx
@@ -1,5 +1,5 @@
 import { useState, useContext, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 import { FaGear } from "react-icons/fa6";
 import { CgProfile } from "react-icons/cg";
 import { IoLogOutOutline } from "react-icons/io5";
@@ -19,6 +19,8 @@ const AsideMenu = () => {
   const [isSettingsOpen, setIsSettingsOpen] = useState(false);
   const [isChatOpen, setIsChatOpen] = useState(false);
   const [menuToggle, setMenuToggle] = useState(window.innerWidth >= 640);
+  const cachedUserData = JSON.parse(localStorage.getItem("userData")) || {};
+  const [userData, setUserData] = useState(cachedUserData);
 
   // Update menuToggle based on screen size
   useEffect(() => {
@@ -75,12 +77,43 @@ const AsideMenu = () => {
 
   const loggedInUserId = getUserIdFromToken();
 
+  useEffect(() => {
+    if (!loggedInUserId) return;
+
+    const fetchUserData = async () => {
+      try {
+        const token = localStorage.getItem("token");
+        if (!token) throw new Error("No token found");
+
+        const response = await fetch(
+          `http://localhost:5001/users/${loggedInUserId}`,
+          {
+            method: "GET",
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
+        if (!response.ok) throw new Error("Failed to fetch user data");
+
+        const data = await response.json();
+        setUserData(data);
+        localStorage.setItem("userData", JSON.stringify(data));
+      } catch (error) {
+        console.error("Error fetching user data:", error);
+      }
+    };
+
+    fetchUserData();
+  }, [loggedInUserId]);
+
   const handleMenuToggle = () => {
     setMenuToggle(!menuToggle);
   };
 
   return (
-    <>
+    <div>
       <button
         onClick={handleMenuToggle}
         className="block sm:hidden fixed top-4 right-4 z-50"
@@ -99,17 +132,34 @@ const AsideMenu = () => {
           className={`flex flex-col fixed sm:sticky w-64 sm:w-50 h-screen top-0 left-0 bg-(--secondary) z-4 
             ${menuToggle ? "block" : "hidden sm:flex"}`}
         >
-          <img
-            src={darkMode ? onlyLogoDM : onlyLogoLM}
-            alt="logo"
-            className="size-20 self-center mt-5 mb-10"
-          />
+          <Link to={`/feed`} className="size-20 self-center mt-10 mb-10">
+            <img src={darkMode ? onlyLogoDM : onlyLogoLM} alt="logo" />
+          </Link>
 
-          <img
-            className="sticky top-0 rounded-full box-content self-center h-20 w-20 p-4"
-            src="https://img.freepik.com/free-vector/smiling-young-man-illustration_1308-173524.jpg?t=st=1739442424~exp=1739446024~hmac=8fdf51934914a44f0c6c6e31f4896f6b8d3ce8e30f70a496347ede2af6634fad&w=740"
-            alt="avatar"
-          />
+          <Link
+            to={`/profile/${loggedInUserId}`}
+            className="w-20 h-20 rounded-full bg-blue-400 flex items-center self-center text-center justify-center text-white"
+          >
+            {userData?.profilePicture ? (
+              <img
+                src={userData.profilePicture}
+                alt="Profile"
+                className="w-full h-full object-cover rounded-full"
+              />
+            ) : (
+              <span>
+                {userData?.username ? userData.username[0].toUpperCase() : "?"}
+              </span>
+            )}
+          </Link>
+
+          <p className="text-center mt-4 mb-2">
+            Welcome back, <br />
+            <Link to={`/profile/${loggedInUserId}`}>
+              <strong> {userData.username}</strong>!
+            </Link>
+          </p>
+
           <button
             onClick={() => handleNavigation("/feed")}
             className="flex items-center pl-5 h-10 hover:bg-(--primary) rounded-full"
@@ -184,9 +234,13 @@ const AsideMenu = () => {
                 setMenuToggle(false);
               }
             }}
-            className="flex absolute bottom-0 left-3 items-center justify-center h-10 hover:bg-(--primary) rounded-full"
+            className="flex absolute bottom-5 pl-5 w-full items-center h-10 hover:bg-(--primary) rounded-full"
           >
-            <IoLogOutOutline size={24} color={darkMode ? "white" : "black"} />
+            <IoLogOutOutline
+              className="mr-1"
+              size={24}
+              color={darkMode ? "white" : "black"}
+            />
             Logout
           </button>
         </motion.div>
@@ -209,7 +263,7 @@ const AsideMenu = () => {
           className="fixed inset-0 bg-black/30 z-30 sm:hidden backdrop-blur-md"
         />
       )}
-    </>
+    </div>
   );
 };
 
