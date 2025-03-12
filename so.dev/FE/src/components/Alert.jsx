4763922d43cf642818cf80ceea7e41aa00d37aa5
contact91 <chouvelquentin91@gmail.com>
2025-03-12 04:32:20 +0100

    alert on delte post fixed

--- so.dev/FE/src/components/Alert.jsx
+++ so.dev/FE/src/components/Alert.jsx
@@ -1,6 +1,6 @@
 import PropTypes from "prop-types";
 
-const Alert = ({ message, onConfirm, isSuccess }) => {
+const Alert = ({ message, onConfirm, onCancel, isSuccess }) => {
   return (
     <div className="fixed inset-0 flex items-center justify-center z-50">
       <div className="fixed inset-0 bg-black opacity-50"></div>
@@ -9,14 +9,25 @@ const Alert = ({ message, onConfirm, isSuccess }) => {
         style={{ backgroundColor: "var(--secondary)" }}
       >
         <p className="text-center mb-6">{message}</p>
-        <div className="flex justify-center">
+        <div className="flex justify-center gap-4">
           <button
             onClick={onConfirm}
             className="px-4 hover:opacity-70 py-2 rounded-lg text-white"
             style={{ backgroundColor: "var(--tertiary)" }}
           >
-            OK
+            {isSuccess ? "OK" : "Confirm"}
           </button>
+          {!isSuccess && onCancel && (
+            <button
+              onClick={onCancel}
+              className="px-4 hover:opacity-70 py-2 text-white rounded-lg"
+              style={{
+                backgroundColor: "var(--quaternary)",
+              }}
+            >
+              Cancel
+            </button>
+          )}
         </div>
       </div>
     </div>
@@ -26,6 +37,7 @@ const Alert = ({ message, onConfirm, isSuccess }) => {
 Alert.propTypes = {
   message: PropTypes.string.isRequired,
   onConfirm: PropTypes.func.isRequired,
+  onCancel: PropTypes.func,
   isSuccess: PropTypes.bool,
 };
 
